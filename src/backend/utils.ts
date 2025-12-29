649410c96acd512258bdf4f6d415bdbbd7d73179
iTrooz <hey@itrooz.fr>
2025-12-29 14:11:52 +0100

    refactor backend code into different classes

---
+++ src/backend/utils.ts
@@ -0,0 +1,30 @@
+import { createWorkersAI } from "workers-ai-provider";
+import { env } from "cloudflare:workers";
+import { z } from "zod";
+
+import { setupLogger } from "./logger";
+
+export const globalLogger = await setupLogger();
+
+export const workersai = createWorkersAI({ binding: env.AI });
+
+export type Result<T, E> = 
+  | { success: true; value: T }
+  | { success: false; error: E };
+
+export const chatModel = workersai("@cf/meta/llama-2-7b-chat-int8");
+export const instructModel = workersai("@cf/meta/llama-3.3-70b-instruct-fp8-fast" as any);
+
+export const introductionSchema = z.object({ // for strict validation
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  interests: z.array(z.string())
+});
+export const partialIntroductionSchema = z.object({ // partial, with descriptions, for partial filling
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  interests: z.array(z.string()).describe("Interests/Hobbies of the user"),
+}).partial();
+export type IntroductionData = z.infer<typeof introductionSchema>;
